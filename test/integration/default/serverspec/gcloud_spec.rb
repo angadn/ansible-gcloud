4c7042fa9621f8ec64aa8a8f4bbaadd7c520b09b
Rui Fernando Hayashi <rfhayashi@gmail.com>
2016-11-23 10:38:01 -0200

    initial implementation

--- test/integration/default/serverspec/gcloud_spec.rb
+++ test/integration/default/serverspec/gcloud_spec.rb
@@ -3,6 +3,12 @@ require 'serverspec'
 # Required by serverspec
 set :backend, :exec
 
-describe command('gcloud') do
+describe command('/usr/local/google-cloud-sdk/bin/gcloud --version') do
   its(:exit_status) { should eq 0 }
+  its(:stdout) { should match /Google Cloud SDK 135\.0\.0/ }
+end
+
+describe file('/etc/profile.d/gcloud.sh') do
+  it { should contain 'export PATH=$PATH:/usr/local/google-cloud-sdk/bin' }
+  it { should be_mode 755 }
 end
